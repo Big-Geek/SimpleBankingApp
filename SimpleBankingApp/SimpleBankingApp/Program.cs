a26f3a132132fae0d73f6b34bdc37dee0fa85984
MarkJonesRepository <61923906+MarkJonesRepository@users.noreply.github.com>
2020-03-07 20:40:17 -0500

    Program and SimpleBankingApp.csproj
    Added main project and program file.

---
+++ SimpleBankingApp/SimpleBankingApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleBankingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
